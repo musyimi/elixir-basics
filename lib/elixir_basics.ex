2fe24e2dda553aa07351c2f172055f7107135928
ngusu <musyimi@users.noreply.github.com>
2022-09-28 14:44:14 +0300

    initial commit

---
+++ lib/elixir_basics.ex
@@ -0,0 +1,18 @@
+defmodule ElixirBasics do
+  @moduledoc """
+  Documentation for `ElixirBasics`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirBasics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
