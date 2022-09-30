d951d3fdc1a24a917d43cbe86b09dc35e7f797a2
ngusu <musyimi@users.noreply.github.com>
2022-09-30 13:06:21 +0300

    processes

---
+++ processes.exs
@@ -0,0 +1,17 @@
+defmodule Demo do
+  def work do
+    :timer.sleep(5000)
+    IO.puts "Result is #{:rand.normal()}"
+  end
+
+  def run do
+    spawn fn ->
+      work()
+    end
+  end
+end
+
+pid = Demo.run
+pid |> IO.inspect
+
+IO.puts "doing some other stuff ....."
